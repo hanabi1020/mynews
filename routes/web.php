fe3a3ea0971bcb3430b80bac4db7587f02e8d838
hanabi <monokuro987@gmail.com>
2022-02-22 02:36:43 +0000

    コミットメッセージ

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+    
+});
+
+Route::group(['prefix' => 'admin','middleware'=>'auth'], function() {
+    Route::get('news/create','Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create','Admin\NewsController@create')->middleware('auth');
+
+
+    // Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
+    // Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
+
+    Route::get('http://XXXXXX.jp/XXX','Admin\AAAController@bbb');
+});
+
+
+Route::group(['prefix' =>'admin'], function() {
+    Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
+    Route::post('profile/create','Admin\ProfileController@create')->middleware('auth');
+    Route::post('profile/edit','Admin\ProfileController@edit')->middleware('auth');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
