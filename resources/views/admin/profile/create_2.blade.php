9cf2c694688b6dea62c2ea86aa997e9da3e51a05
hanabi <monokuro987@gmail.com>
2022-07-17 21:43:28 +0000

    課題

--- resources/views/admin/profile/create_2.blade.php
+++ resources/views/admin/profile/create_2.blade.php
@@ -1,16 +1,16 @@
 {{-- layouts/admin.blade.phpを読み込む --}}
-@extends('layouts.admin_2')
+@extends('layouts.profile_2')
 
 
 {{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', 'ニュースの新規作成_2')
+@section('title', 'Myプロフィール_2')
 
 {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>ニュース新規作成_2</h2>
+                <h2>Myプロフィール_2</h2>
             </div>
         </div>
     </div>
