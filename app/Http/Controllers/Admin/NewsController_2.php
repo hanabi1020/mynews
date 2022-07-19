ba470bf380850ee57b0de71a468c9a4008cb0ccf
hanabi <monokuro987@gmail.com>
2022-07-19 16:01:49 +0000

    課題

--- app/Http/Controllers/Admin/NewsController_2.php
+++ app/Http/Controllers/Admin/NewsController_2.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\News_2;
+
 class NewsController_2 extends Controller
 {
     public function add()
@@ -13,6 +15,29 @@ class NewsController_2 extends Controller
   }
   public function create(Request $request)
   {
+      
+       // Varidationを行う
+      $this->validate($request, News_2::$rules);
+​
+      $news_2 = new News_2
+      $form = $request->all();
+​
+      // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $news_2->image_path = basename($path);
+      } else {
+          $news_2->image_path = null;
+      }
+​
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['image']);
+​
+      // データベースに保存する
+      $news_2->fill($form);
+      $news_2->save();
       // admin/news/createにリダイレクトする
       return redirect('admin/news/create');
   } 
--- app/Http/Controllers/Admin/ProfileController_2.php
+++ app/Http/Controllers/Admin/ProfileController_2.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile_2;
+
 class ProfileController_2 extends Controller
 {
     public function add()
@@ -14,6 +16,22 @@ class ProfileController_2 extends Controller
 
     public function create()
     {
+        
+        // dd($request);
+        // Varidationをおこなう
+        $this->validate($request,Profile_2::$rules);
+        
+        $profile_2 = new Profile_2();
+        $form = $request->all();
+        
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+      
+        // データベースに保存する
+        $profile_2->fill($form);
+        $profile_2->save();
+      
+        
         return redirect('admin/profile/create_2');
     }
 
